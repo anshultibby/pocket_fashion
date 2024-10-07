7336b9118877ae83440b05d32905a9144e10683f
Anshul <anshul@notablehealth.com>
2024-10-07 01:04:59 -0700

    backend works well, need to precompute all text attributes

--- backend/modules/classify.py
+++ backend/modules/classify.py
@@ -10,7 +10,8 @@ processor = AutoProcessor.from_pretrained('Marqo/marqo-fashionSigLIP', trust_rem
 styles = pd.read_csv(f"data/datasets/all_styles_processed.csv")
 style_dict = {label_type: list(group['label_value'].unique()) \
               for label_type, group in styles.groupby('label_type')}
-THRESHOLD = 0.3
+RELATIVE_THRESHOLD = 0.8
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -36,15 +37,21 @@ def main():
 
     text_probs = {}
     for label_type, label_values in style_dict.items():
+        # Convert label_values to strings
+        label_values = [str(value) for value in label_values]
         processed = processor(text=label_values, images=image, padding='max_length', return_tensors="pt")
         text_features = model.get_text_features(processed['input_ids'], normalize=True)
-        text_probs[label_type] = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-    
-    for label_type, label_values in style_dict.items():
-        for label_value in label_values:
-            if text_probs[label_type][0][label_value] > THRESHOLD:
-                print(f"{label_type}: {label_value}")
+        text_probs[label_type] = (100.0 * image_features @ text_features.T).softmax(dim=-1).squeeze(0)
 
+    for label_type, probs in text_probs.items():
+        max_prob, max_index = torch.max(probs, dim=0)
+        max_prob_value = max_prob.item()
+        
+        print(f"\n{label_type.capitalize()}:")
+        for i, (label_value, prob) in enumerate(zip(style_dict[label_type], probs)):
+            prob_value = prob.item()
+            if prob_value == max_prob_value or prob_value >= max_prob_value * RELATIVE_THRESHOLD:
+                print(f"  {label_value}: {prob_value:.2f}%")
 
 if __name__ == "__main__":
     main()
